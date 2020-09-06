9c90a77c52a71fb46d55137a721f27adf30389fd
EricksonLopes <ofc.erickson@gmail.com>
2020-09-06 18:50:44 -0300

    * Debugger is active!

--- Quickstart/app.py
+++ Quickstart/app.py
@@ -11,3 +11,22 @@ def hello_world():
 @app.route('/hello')
 def hello():
     return 'Hello'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'Username: {username}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {subpath}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
